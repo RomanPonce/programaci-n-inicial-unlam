f4b92454ee4ca67dc19fbd3a15f765eb49462420
roman <romanponce202@gmail.com>
2024-08-27 22:09:55 -0300

    Más cambiossss

---
+++ Practicas/03_Iteracion/iteracion_condicional.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+    int pow;
+
+    pow = 2;
+
+    while(pow<600){
+        printf("%d\n",pow);
+        pow = pow*2;
+    }*/
+
+    int i, day, month;
+
+    i = 0;
+
+    while(i!=1){
+
+        printf("Digite un dia entre 1 y 31: ");
+        scanf("%d",&day);
+
+        printf("Digite un mes entre 1 y 12: ");
+        scanf("%d",&month);
+
+        if(month>12 || day > 31){
+            printf("\nError. El programa detecto fechas invalidas.\nDigite las fechas otra vez\n");
+        }
+        else{
+            printf("\nEl programa se ejecuto correctamente. Buen trabajo\n ");
+            i++;
+        }
+
+    }
+
+
+
+    return 0;
+}

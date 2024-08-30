cc67fa5f96b6b455fc2e2dda9890f53d43d15753
roman <romanponce202@gmail.com>
2024-08-29 23:24:00 -0300

    Cambios

--- Practicas/03_Iteracion/iteracion_condicional.c
+++ Practicas/03_Iteracion/iteracion_condicional.c
@@ -2,6 +2,52 @@
 
 int main()
 {
+    char digit;
+    int count_numbers=0,count_letters=0,count_letters_capitalize=0,count_letters_minus=0,count_general=0,total;
+
+    while(digit!='*'){
+
+        printf("Digite un caracter: ");
+        scanf(" %c",&digit);
+
+        if(digit >= 48 && digit <= 57){
+            count_numbers++;
+        }
+        else if(digit >= 65 && digit <= 90){
+            count_letters++;
+            count_letters_capitalize++;
+        }
+        else if(digit >= 97 && digit <= 122){
+            count_letters++;
+            count_letters_minus++;
+        }
+        else{
+            count_general++;
+        }
+    }
+
+    count_general -= 1;
+    total = count_general + count_letters + count_numbers;
+
+    printf("\n");
+    printf("Total de caracteres ingresados:    %d\n",total);
+    printf("Caracteres numericos:              %d\n",count_numbers);
+    printf("Caracteres alfabeticos:            %d\n",count_letters);
+    printf("Caracteres alfabeticos mayusculas: %d\n",count_letters_capitalize);
+    printf("Caracteres alfabeticos minusculas: %d\n",count_letters_minus);
+    printf("Otros caracteres:                  %d\n",count_general);
+
+
+
+
+
+
+
+
+
+
+
+
     /*
     int pow;
 
